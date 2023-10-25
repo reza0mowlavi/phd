4791ce88c2c0f5269718f13552576630d0ce1c67
Reza Mowlavi <reza0mowlavi@gmail.com>
2023-10-26 01:24:06 +0330

    OversampledDataset

--- dataset.py
+++ dataset.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from PIL import Image
 
+import numpy as np
+
 import torch
 
 
@@ -14,14 +16,68 @@ class Dataset(torch.utils.data.Dataset):
             self.cache()
 
     def cache(self):
-        self.images = [Image.open(Path(path)) for path in self.paths]
+        self.images = {path: Image.open(Path(path)) for path in self.paths}
 
     def __len__(self):
         return len(self.paths)
 
     def __getitem__(self, index):
-        path = self.paths
-        img = self.images[index] if self.cached else Image.open(Path(path))
+        path = self.paths[index]
+        img = self.images[path] if self.cached else Image.open(Path(path))
         img = self.transform(img) if self.transform is not None else img
         label = self.labels[index]
         return img, label
+
+
+class OversampledDataset(torch.utils.data.IterableDataset):
+    def __init__(self, paths, labels, cached=True, transform=None, max_steps=None):
+        self.paths = paths
+        self.labels = labels
+        self.cached = cached
+        self.transform = transform
+        self.max_steps = max_steps
+        self.iterable_datasets = [
+            IterableDataset(
+                paths=paths[labels == unique_label],
+                labels=labels[labels == unique_label],
+                cached=cached,
+                transform=transform,
+            )
+            for unique_label in np.unique(labels)
+        ]
+
+    def __iter__(self):
+        step = 1
+        for elements in zip(*self.iterable_datasets):
+            yield elements
+            if self.max_steps is not None and self.max_steps < step:
+                break
+            else:
+                step += 1
+
+
+class IterableDataset(torch.utils.data.IterableDataset):
+    def __init__(self, paths, labels, cached=True, transform=None):
+        self.paths = paths
+        self.labels = labels
+        self.cached = cached
+        self.transform = transform
+        if self.cached:
+            self.cache()
+
+    def cache(self):
+        self.images = {path: Image.open(Path(path)) for path in self.paths}
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __iter__(self):
+        indices = np.arange(len(self))
+        while True:
+            np.random.shuffle(indices)
+            for index in indices:
+                path = self.paths[index]
+                img = self.images[path] if self.cached else Image.open(Path(path))
+                img = self.transform(img) if self.transform is not None else img
+                label = self.labels[index]
+                yield img, label
